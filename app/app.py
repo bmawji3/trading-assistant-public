ee7c2815f9275fe5492f3dedc92a88940dbd3a1e
Bilal <bmawji3@gatech.edu>
2021-10-27 22:17:15 -0500

    Add sample AWS utils

--- app/app.py
+++ app/app.py
@@ -1,7 +1,8 @@
 import json
 import os
 
-from utils.util import gather_data
+from utils.aws_util import *
+from utils.data_util import gather_data
 
 
 def main():
@@ -26,6 +27,12 @@ def test_gather_data():
             spaces = " ".join(array)
             spaces_array.append(spaces)
         gather_data(symbols_array, spaces_array)
+
+
+def test_s3_upload():
+    s3 = get_s3_resource()
+    upload_file_to_bucket(s3, 'my-test-bucket', os.path.join(os.getcwd(), 'data', 'AAPL.csv'))
+    list_bucket_objects(s3, 'my-test-bucket')
 
 
 if __name__ == '__main__':
