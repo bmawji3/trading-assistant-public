7d65e92206ac00ab49c2ff50c9bc3557c5ecee5b
khaledfouad0 <khaled.h.fouad@gmail.com>
2021-11-03 08:51:42 +0300

    added examples to use the data_util and indicators

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -1,6 +1,8 @@
 import datetime as dt
 import pandas as pd
 import data_util as util
+import indicators as ta
+
 
 if __name__ == '__main__':
     # main()
@@ -9,9 +11,18 @@ if __name__ == '__main__':
     end_date = dt.datetime(2021, 4, 27)
     dates = pd.date_range(start_date,end_date)
 
-    prices_df = util.get_prices(symbols, dates)
-    prices_normed = util.normalize_prices(prices_df)
+    # prices_df = util.get_closings(symbols, dates)
+    # prices_normed = util.normalize(prices_df)
     # plot_data(prices_normed)
 
-    FB_stock_data = util.clean_data("FB", dates)
+    FB_stock_data = util.get_ohlcv("FB", dates)
     # print(FB_stock_data)
+
+    # sma_FB = ta.sma(FB_stock_data,window=5)
+    # print(sma_FB.columns)
+    
+    # ema_FB = ta.ema(FB_stock_data,window=5)
+    # print(ema_FB)
+
+    vama_FB = ta.vama(FB_stock_data,window=5)
+    print(vama_FB)
